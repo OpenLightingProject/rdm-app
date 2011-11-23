0ed4915fdba130f0e9fcf7d4f8a87c9c481c9c99
Simon Newton <nomis52@gmail.com>
2011-11-23 08:48:50 -0800

    * switch to png as they handle transparency

--- image_fetcher.py
+++ image_fetcher.py
@@ -61,9 +61,9 @@ class ImageFetcher(object):
       img.resize(width=self.RESIZE_WIDTH)
     # we need at least one transform, so use I'm feeling lucky :)
     img.im_feeling_lucky();
-    thumbnail = img.execute_transforms(output_encoding=images.JPEG)
+    thumbnail = img.execute_transforms(output_encoding=images.PNG)
 
-    file_name = files.blobstore.create(mime_type='image/jpeg')
+    file_name = files.blobstore.create(mime_type='image/png')
     with files.open(file_name, 'a') as f:
       f.write(thumbnail)
     files.finalize(file_name)
