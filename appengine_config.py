faa94e7f32ad0fbc0ef93bc406faaa66648f1ef2
Simon Newton <nomis52@gmail.com>
2012-01-22 12:21:20 -0800

    * use the app stats module

--- appengine_config.py
+++ appengine_config.py
@@ -1,4 +1,26 @@
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# appengine_config.py
+# Copyright (C) 2012 Simon Newton
 
 # use django 1.2
 from google.appengine.dist import use_library
 use_library('django', '1.2')
+
+
+def webapp_add_wsgi_middleware(app):
+  from google.appengine.ext.appstats import recording
+  app = recording.appstats_wsgi_middleware(app)
+  return app
