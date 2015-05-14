3c403b91d6c2740bd004d14e2390b4d8155d79e4
Dave Olsthoorn <dave.olsthoorn@gmail.com>
2015-05-14 12:24:02 +0200

    install bower as user

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,6 +5,6 @@ if [[ $TASK = 'nosetests' ]]; then
 elif [[ $TASK = 'karma' ]]; then
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start
-    sudo npm install -g grunt-cli
+    npm install -g grunt-cli
     npm start
 fi
