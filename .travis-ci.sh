9ec6d7b3de4b3bd50075d422d1813b8e340c3155
Dave Olsthoorn <dave.olsthoorn@gmail.com>
2015-07-11 18:57:51 +0200

    move install instructions

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 if [[ $TASK = 'nosetests' ]]; then
-    pip install json-spec
+#    pip install json-spec
     nosetests
 elif [[ $TASK = 'karma' ]]; then
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start
-    npm install -g grunt-cli
-    npm install
+#    npm install -g grunt-cli
+#    npm install
     grunt unit-test
 elif [[ $TASK = 'js-lint' ]]; then
-    npm install -g grunt-cli
-    npm install
+#    npm install -g grunt-cli
+#    npm install
     grunt lint
 fi
