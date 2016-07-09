bd3d42c968bb7fc4b588856661f3a9a5d78558dc
Peter Newman <peterjnewman@gmail.com>
2016-07-09 02:23:53 +0100

    Update from source to fix up missing escape characters around quotes

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -52,7 +52,7 @@ MANUFACTURER_DATA = [
 (0x0303, "Shanghai Moons' Automation Control Co., Ltd"),
 (0x0306, "feno GmbH"),
 (0x0308, "ImageCue LLC "),
-(0x030F, "Guangzhou Litewise Lighting Equipments Co., Ltd. dba "EK Lights""),
+(0x030F, "Guangzhou Litewise Lighting Equipments Co., Ltd. dba \"EK Lights\""),
 (0x032C, "Carallon Ltd."),
 (0x033A, "Lux Lumen"),
 (0x034B, "Rosstech Signals Inc."),
