b2cd61e3fbdd2d4e0703f0c3b7c1786f434a9fb2
Peter Newman <peternewman@users.noreply.github.com>
2017-03-03 14:10:29 +0000

    Workaround flaws in recode

--- tools/make_manufacturer_data.sh
+++ tools/make_manufacturer_data.sh
@@ -28,6 +28,10 @@ EOM
 echo -n "MANUFACTURER_DATA = "
 
 # Fetch the manufacturer data, convert to Linux line endings
+# Bodge for bugs in recode (recode takes "foo & bar" and incorrectly converts
+# it to "foo  bar", whereas "foo &amp; bar" correctly gets converted to
+# "foo & bar"). We can't find every instance of an ampersand that's not an
+# entity so we cheat.
 # Convert any HTML encoding to Latin 1 using recode
 # Tidy nbsp to space
 # Convert extended characters to closest normal equivalent
@@ -39,6 +43,7 @@ echo -n "MANUFACTURER_DATA = "
 # TODO(Peter): Comment out any invalid rows
 wget --quiet -O - http://tsp.esta.org/tsp/working_groups/CP/rdmids.php | \
 tr --delete "\r" | \
+sed -r -e 's/([[:space:]]+)&([[:space:]]+)/\1&amp;\2/' | \
 recode html..latin1 | \
 tr "\240" " " | \
 tr "\300-\305" "[A*]" | tr "\310-\313" "[E*]" | tr "\314-\317" "[I*]" | tr "\322-\326" "[O*]" | tr "\331-\334" "[U*]" | \
