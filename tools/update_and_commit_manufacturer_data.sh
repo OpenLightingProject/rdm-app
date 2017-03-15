6ee89d42c8028b500326cf860ad162355f6812d3
Peter Newman <peterjnewman@gmail.com>
2017-03-15 15:10:57 +0000

    Add some checks of the data before committing it

--- tools/update_and_commit_manufacturer_data.sh
+++ tools/update_and_commit_manufacturer_data.sh
@@ -22,6 +22,7 @@ fi
 date
 
 cd "${BASH_SOURCE%/*}" || exit # cd into the bundle and use relative paths
+cd ../ || exit
 
 echo "Pulling from $ORIGIN $ORIGIN_BRANCH"
 git pull $ORIGIN $ORIGIN_BRANCH || exit
@@ -30,10 +31,27 @@ echo "Pulling from $UPSTREAM $UPSTREAM_BRANCH"
 git pull $UPSTREAM $UPSTREAM_BRANCH || exit
 
 echo "Updating data"
-./make_manufacturer_data.sh > ../data/manufacturer_data.py
+tools/make_manufacturer_data.sh > data/manufacturer_data.py
+
+echo "Sanity checks"
+
+echo "Generated file length..."
+if [ `cat data/manufacturer_data.py | wc -l` -lt 30 ]
+then
+echo "Failed to fetch data properly, not enough lines"
+exit
+fi
+
+echo "File content..."
+python data/manufacturer_test.py
+if [ $? -ne 0 ]
+then
+echo "Failed to fetch data properly, issues with file content"
+exit
+fi
 
 echo "Checking for differences"
-git diff --exit-code ../data/manufacturer_data.py
+git diff --exit-code data/manufacturer_data.py
 
 if [ $? -eq 0 ]
 then
@@ -42,7 +60,7 @@ exit
 fi
 
 echo "Committing"
-git commit -m "`printf "Autoupdated manufacturer data\nPlease check this manually before merging"`" ../data/manufacturer_data.py
+git commit -m "`printf "Autoupdated manufacturer data\nPlease check this manually before merging"`" data/manufacturer_data.py
 
 echo "Pushing to $ORIGIN $ORIGIN_BRANCH"
 git push $ORIGIN $ORIGIN_BRANCH || exit
