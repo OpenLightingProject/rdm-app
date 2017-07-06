fd828bc66b97f7391953e71bc0dc1a13fdf52968
Peter Newman <peterjnewman@gmail.com>
2017-07-06 17:27:05 +0100

    Autoupdated manufacturer data Please check this manually before merging

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -42,6 +42,7 @@ MANUFACTURER_DATA = [
 (0x0111, "Radig Hard & Software"),
 (0x0112, "RGB Lighting Equipment  Co., Ltd."),
 (0x0113, "Airstar SAS"),
+(0x0114, "BSL Lighting"),
 (0x016C, "LGR"),
 (0x01C8, "Guangzhou JINLIN Stage Lighting Equipment Co., Ltd. "),
 (0x01C9, "ALPHA LITE Inc."),
