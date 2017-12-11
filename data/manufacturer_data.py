86fdc82f42708a1566ec298ccc3f50bf10f6c3d9
Peter Newman <peterjnewman@gmail.com>
2017-12-11 14:27:05 +0000

    Autoupdated manufacturer data Please check this manually before merging

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -83,6 +83,7 @@ MANUFACTURER_DATA = [
 (0x01DA, "Guangzhou Daisy Electronic Technology Co., Ltd. "),
 (0x01DB, "Logen Ltd."),
 (0x01DC, "LED Linear GmbH"),
+(0x01DD, "Photonia srl"),
 (0x0202, "Paul Heuts"),
 (0x0203, "Artemide S.p.A"),
 (0x0204, "LIGHTLINE Lasertechnik GmbH"),
