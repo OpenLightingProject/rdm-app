5b10a9bb1f098778716241ef4d88e20e8c3d5e80
Peter Newman <peternewman@users.noreply.github.com>
2017-12-27 22:53:04 +0000

    Add the spellchecker task

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -9,4 +9,18 @@ elif [[ $TASK = 'js-lint' ]]; then
     grunt --verbose lint
 elif [[ $TASK = 'data-check' ]]; then
     ./tools/make_manufacturer_data.sh > data/manufacturer_data.py && git diff --exit-code data/manufacturer_data.py
+elif [[ $TASK = 'spellintian' ]]; then
+  # run the spellchecker only if it is the requested task
+  spellingfiles=$(find ./ -type f -and ! \( \
+      -wholename "./.git/*" -or \
+      \) | xargs)
+  # count the number of spellchecker errors
+  spellingerrors=$(zrun spellintian $spellingfiles 2>&1 | wc -l)
+  if [[ $spellingerrors -ne 0 ]]; then
+    # print the output for info
+    zrun spellintian $spellingfiles
+    echo "Found $spellingerrors spelling errors"
+    exit 1;
+  else
+    echo "Found $spellingerrors spelling errors"
 fi
