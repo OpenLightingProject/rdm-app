56251394f7e7917975c4d7f133ba1fd5238b43cf
Peter Newman <peternewman@users.noreply.github.com>
2017-12-27 23:47:22 +0000

    Ignore node_modules

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -12,7 +12,8 @@ elif [[ $TASK = 'data-check' ]]; then
 elif [[ $TASK = 'spellintian' ]]; then
   # run the spellchecker only if it is the requested task
   spellingfiles=$(find ./ -type f -and ! \( \
-      -wholename "./.git/*" \
+      -wholename "./.git/*" -or \
+      -wholename "./node_modules/*" \
       \) | xargs)
   # count the number of spellchecker errors
   spellingerrors=$(zrun spellintian $spellingfiles 2>&1 | wc -l)
