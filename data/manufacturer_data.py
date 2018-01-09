b8d853dc3ff99e9b147fa9837067e4c5e1e382d7
Peter Newman <peterjnewman@gmail.com>
2018-01-09 17:27:05 +0000

    Autoupdated manufacturer data Please check this manually before merging

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -34,6 +34,7 @@ MANUFACTURER_DATA = [
 (0x00AD, "ColorDeve Co. Limited"),
 (0x00AE, "Vitrulux Ltd"),
 (0x00AF, "NanoPLC LLC"),
+(0x00B0, "ARENA LUCI s.r.l."),
 (0x0101, "St. Anne Engineering GmbH"),
 (0x0102, "Bortis Elektronik"),
 (0x0103, "Fontana Technologies"),
