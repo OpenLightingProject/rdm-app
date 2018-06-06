2d23210e1184a1941b600844bcb5c01a61df0946
Peter Newman <peterjnewman@gmail.com>
2018-06-06 20:28:14 +0100

    Autoupdated manufacturer data Please check this manually before merging

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -274,6 +274,7 @@ MANUFACTURER_DATA = [
 (0x0884, "Bright Group"),
 (0x0885, "SIRS-E"),
 (0x088A, "Highendled Electronics Company Limited"),
+(0x088E, "Stage One International Co., Ltd."),
 (0x088F, "First Design System Inc."),
 (0x0891, "Feiner Lichttechnik GMBH"),
 (0x089A, "ADL Electronics Ltd."),
