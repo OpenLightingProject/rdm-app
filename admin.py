3eb93cd8ef814088f30003c5786d5a37fd559e19
Peter Newman <peterjnewman@gmail.com>
2018-07-28 17:49:37 +0100

    Populate the footer stats in the admin pages

--- admin.py
+++ admin.py
@@ -17,6 +17,7 @@
 # The handlers for the admin page.
 
 import common
+from common import BasePageHandler
 from data.controller_data import CONTROLLER_DATA
 from data.manufacturer_data import MANUFACTURER_DATA
 from data.manufacturer_links import MANUFACTURER_LINKS
@@ -61,7 +62,7 @@ def UpdateModificationTime(timestamp_name):
   memcache.delete(memcache_keys.INDEX_INFO)
 
 
-class BaseAdminPageHandler(webapp.RequestHandler):
+class BaseAdminPageHandler(BasePageHandler):
   """The base handler for admin requests."""
   ALLOWED_USERS = [
       'nomis52@gmail.com',
@@ -523,10 +524,9 @@ class AdminPageHandler(BaseAdminPageHandler):
 
     pending_uploads = UploadedResponderInfo.all().filter('processed = ',
                                                          False).count()
-    template_data = {
-        'logout_url': users.create_logout_url("/"),
-        'responders_to_moderate': pending_uploads,
-    }
+    template_data = self.IndexInfo()
+    template_data['logout_url'] = users.create_logout_url("/")
+    template_data['responders_to_moderate'] = pending_uploads
 
     if output:
       template_data['output'] = output
