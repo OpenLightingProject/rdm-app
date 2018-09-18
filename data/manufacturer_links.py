466ff9351c3bbc1cee3a1750186b4f6cd12f0e1f
Peter Newman <peternewman@users.noreply.github.com>
2018-09-18 03:42:59 +0100

    Add Guangzhou Baiyun District Sanjie Electronic Stage Lighting Audio Equipment Factory URL

--- data/manufacturer_links.py
+++ data/manufacturer_links.py
@@ -25,6 +25,7 @@ MANUFACTURER_LINKS = [
   (0x00A8, "https://www.eye.co.jp/"),
   # (0x00A9, "http://www.richter-lt.de"), # Temporarily(?) down
   (0x00B0, "http://www.arenaluci.com/"),
+  (0x00B9, "http://en.gzsjlight.com/"),
   (0x0104, "https://www.blizzardlighting.com/"),
   (0x01CC, "https://portmanlights.com/"),
   (0x01CD, "http://www.compulite.com/"),
