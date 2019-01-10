7124dc05add41d83973ae063851cad418ad5c0dd
Peter Newman <peterjnewman@gmail.com>
2019-01-10 23:29:11 +0000

    Autoupdated manufacturer data Please check this manually before merging

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -23,6 +23,7 @@ MANUFACTURER_DATA = [
   (0x0000, "ESTA"),
   (0x0001, "GEE"),
   (0x0088, "Guangzhou YaFeng Optoelectronic Equipment Co., Ltd."),
+  (0x0089, "The Production Warehouse (Vulcan Lighting)"),
   (0x009A, "Guangzhou Haoyang Electronic Co., Ltd."),
   (0x009B, "Guangzhou Jiawei Electronic Technology Co., Ltd."),
   (0x009E, "Guangzhou Flying Butterfly Stage Lighting Equipment Co., Ltd."),
