e76b3e5516ccb0bea16408f38ca26e8ee9185957
Peter Newman <peterjnewman@gmail.com>
2019-06-18 12:29:34 +0100

    Autoupdated manufacturer data Please check this manually before merging

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -378,6 +378,7 @@ MANUFACTURER_DATA = [
   (0x088F, "First Design System Inc."),
   (0x0890, "Taurus Light Co.,Limited"),
   (0x0891, "Feiner Lichttechnik GMBH"),
+  (0x0893, "Brighten LED Lighting Limited"),
   (0x089A, "ADL Electronics Ltd."),
   (0x089D, "gobo.ws"),
   (0x08A1, "Shenzhen Gloshine Technology Co., Ltd"),
