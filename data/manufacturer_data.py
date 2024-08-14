75645778e962b940fcbf3642b9b53434c7253d3e
Peter Newman <peterjnewman@gmail.com>
2024-08-14 10:29:39 +0100

    Autoupdated manufacturer data Please check this manually before merging

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -765,6 +765,7 @@ MANUFACTURER_DATA = [
   (0x091F, "Yaham Recience Technology Co,. ltd."),
   (0x0920, "Vexica Technology Limited"),
   (0x0921, "Guangzhou mengyi stage lighting equipment co., LTD."),
+  (0x0922, "von Gaisberg Engineering"),
   (0x0923, "Hangzhou Roleds Lighting System Co., Ltd."),
   (0x0925, "Guangzhou Aiweidy Lighting Acoustics Equipment Co.,Ltd."),
   (0x0926, "Elumeros Lighting Limited"),
